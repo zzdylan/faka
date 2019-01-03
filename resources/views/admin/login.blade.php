68d62c606efc50f6b9ce45d9a51f39639307f85a
dylan <527844046@qq.com>
2019-01-03 18:08:34 +0800

    demo

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -40,7 +40,7 @@
           @endforeach
         @endif
 
-        <input type="input" class="form-control" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username') }}">
+        <input type="input" class="form-control" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username','admin') }}">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback {!! !$errors->has('password') ?: 'has-error' !!}">
@@ -51,7 +51,7 @@
           @endforeach
         @endif
 
-        <input type="password" class="form-control" placeholder="{{ trans('admin.password') }}" name="password">
+        <input type="password" class="form-control" placeholder="{{ trans('admin.password') }}" name="password" value="admin">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="row">
