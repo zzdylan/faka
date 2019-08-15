2e2bf5eecd86d301126fe177579c71f3fe7743d7
zzdylan <527844046@qq.com>
2019-08-15 11:21:42 +0800

    detail

--- app/Admin/Controllers/OrderController.php
+++ app/Admin/Controllers/OrderController.php
@@ -174,7 +174,7 @@ class OrderController extends Controller
                     $string = $string . "\r\n";
                 }
             }
-            return "<textarea>{$string}</textarea>>";
+            return "<textarea>{$string}</textarea>";
         });;
         $show->created_at('创建时间');
 
